d40e1ac912bb4d2d86dac35882a315ce3470c918
Nemika-Haj <nemika@bytestobits.dev>
2025-11-20 02:04:06 +0200

    Add shamrock

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import Loader from "@/components/ui/loader";
-import { AbsoluteCenter, Box } from "@chakra-ui/react";
+import { AbsoluteCenter, Box, Icon, IconButton, useDisclosure } from "@chakra-ui/react";
 import { useSession } from "next-auth/react";
 import MainLayout from "@/components/layouts/MainLayout";
+import { FaLock } from "react-icons/fa";
+import Shamrock from "@/components/shamrock";
 
 export default function Index() {
-  const session = useSession();
+  const session = useSession()
+  const shamrockDisclosure = useDisclosure()
 
   return (
     <>
@@ -27,6 +30,22 @@ export default function Index() {
           >
 
           </Box>
+
+          <IconButton
+            position="absolute"
+            left={0}
+            top={"230px"}
+            background="orange.500"
+            roundedRight="md"
+            border="none"
+            size="2xs"
+            height="80px"
+            onClick={shamrockDisclosure.onOpen}
+          >
+            <FaLock />
+          </IconButton>
+
+          <Shamrock isOpen={shamrockDisclosure.open} onClose={shamrockDisclosure.onClose} />
         </MainLayout>
       )}
     </>
