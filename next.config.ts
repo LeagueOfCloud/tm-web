f4acd1aeeb377a01055fb555e07b4a832ba95bec
Nemika-Haj <nemika@bytestobits.dev>
2026-02-06 17:02:44 +0200

    Directly expose variables in nextconfig for Amplify support

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"]
+  },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+    DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID
   }
 };
 
